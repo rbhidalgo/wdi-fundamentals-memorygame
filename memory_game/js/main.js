ff12c864a7bd2bf7a398228a6f68103048f3189f
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-03-10 11:37:02 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,18 +3,19 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again");
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
 }
+
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
